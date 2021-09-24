d783d93e82127e316c33009ef6d988d69daf1a46
bibekkarki067 <bibekkarki067@gmail.com>
2021-09-24 10:49:15 -0500

    Script to get content to populate DB

---
+++ CornBot/ML/Scripts/getURls.py
@@ -0,0 +1,27 @@
+"""
+Created on 9/24/2021
+@author: Bibek
+"""
+import pandas as pd
+import os
+
+def get_filenames_urls_labels():
+    """
+        Returns a tuple of lists of filenames,urls and labels in order.
+        Returns
+        -------
+        returnValues : tuple
+            Lists.
+    """
+    path = 's3://cornimagesbucket/csvOut.csv'# Path to the S3 bucket
+    data = pd.read_csv(path, index_col = 0, header = None)#Read the csv
+    data_temp = data.reset_index()#Recover the original index
+    image_src = "cornimagesbucket.s3.us-east-2.amazonaws.com/images_compressed/"
+    filenames = list(data_temp.iloc[:,0])#Get all the filename
+    labels = list(data.iloc[:,-1])#Get corrosponding Labels of the filename
+    file_urls = []
+    for filename in filenames:
+        file_urls.append(os.path.join(image_src,filename))#Src + filename is fileUrl
+    return filenames,file_urls,labels
+
+
