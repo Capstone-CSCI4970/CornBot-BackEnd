8432c4b72fe78a6d65eef3c4b41bef71e3d3068b
bibekkarki067 <bibekkarki067@gmail.com>
2021-10-01 18:35:31 -0500

    populate Imagetable(stores urls+labels)

---
+++ CornBot/train_cornbot/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class ImageTable(models.Model):
+    imagename = models.CharField(db_column='ImageName', primary_key=True, max_length=512)  # Field name made lowercase.
+    imageurl = models.CharField(db_column='ImageUrl', unique=True, max_length=512)  # Field name made lowercase.
+    label = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'image_table'
