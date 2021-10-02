90f01561a3e8e984cdddb84feb46010c64b3cf42
bibekkarki067 <bibekkarki067@gmail.com>
2021-10-01 19:20:54 -0500

    Apiendpoint for view image(get first 5)

--- CornBot/train_cornbot/views.py
+++ CornBot/train_cornbot/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import ImageTable
+from .serializers import ImageTableSerializer
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def image_list(request):
+    # Get list of tutorials, post a new tutorial, delete all tutorials
+    if request.method == 'GET':
+        images = ImageTable.objects.all()[:5]
+        image_serializer = ImageTableSerializer(images, many=True)
+        return JsonResponse(image_serializer.data, safe=False)
+        # safe=False for objects serialization
