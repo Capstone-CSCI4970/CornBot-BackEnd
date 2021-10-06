139ac4730c94f273e4d2fac608b4b5b6c955e3cb
bibekkarki067 <bibekkarki067@gmail.com>
2021-10-06 16:48:10 -0500

    setup mysql

--- CornBot/CornBot/settings.py
+++ CornBot/CornBot/settings.py
@@ -84,10 +84,14 @@ WSGI_APPLICATION = 'CornBot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': '<databasename>',
+        'USER': 'root',
+        'PASSWORD': '<pwd>',
+        'HOST': 'localhost', 
+        'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
