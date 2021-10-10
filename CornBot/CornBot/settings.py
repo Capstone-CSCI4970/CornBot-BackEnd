e9ee5535277ad6e2424430ddb2cf24dc27d00dbf
Cody <codyroe72@gmail.com>
2021-10-10 14:24:11 -0500

    Update settings.py
    Updated settings to add the API app into the overall project, commented out mysql database settings and included database settings for sqlite so that both could be used locally by just commenting one of the two database settings depending on which one is being used locally

--- CornBot/CornBot/settings.py
+++ CornBot/CornBot/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework.authtoken',
     'train_cornbot.apps.TrainCornbotConfig',
+    'API.apps.ApiConfig'
 
 ]
 
@@ -84,17 +85,23 @@ WSGI_APPLICATION = 'CornBot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': '',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost', 
+#         'PORT': '3306',
+#         }
+#     }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': '',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost', 
-        'PORT': '3306',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
