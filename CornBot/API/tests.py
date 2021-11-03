7713206c57b8b0a9b6428bbc87c089d0060a92d0
Cody <codyroe72@gmail.com>
2021-11-03 18:52:43 -0500

    Added basic test case
    Created a basic test case for user registration to show the testing tool we will be using for our back-end project

--- CornBot/API/tests.py
+++ CornBot/API/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from tutorials.models import Tutorial
+from tutorials.serializers import TutorialSerializer
+
+
+class RegistrationTestCase(APITestCase):
+
+    def test_registration(self):
+
+        print("Testing user registration")
+        data = {
+            "username": "testcase",
+            "email" : "testcase@email.com",
+            "password": "testcasepassword"
+        }
+        response = self.client.post("/api/users/" , data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED) # example of test passing
+        # self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT) # example of test failing
+
