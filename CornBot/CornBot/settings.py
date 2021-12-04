b7050f5c3547b15b1a9b7b70a159c8a1969eaaa2
Cody <codyroe72@gmail.com>
2021-12-04 13:25:43 -0600

    Update settings.py
    Update settings.py to remove extra mysql database dictionary, commented mysql for sqlite as a better default database for master

--- CornBot/CornBot/settings.py
+++ CornBot/CornBot/settings.py
@@ -82,6 +82,7 @@ WSGI_APPLICATION = 'CornBot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql', 
@@ -95,23 +96,10 @@ WSGI_APPLICATION = 'CornBot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost', 
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-#
-# =======
-# # DATABASES = {
-# #     'default': {
-# #         'ENGINE': 'django.db.backends.sqlite3',
-# #         'NAME': BASE_DIR / 'db.sqlite3',
-# #     }
-# # }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
